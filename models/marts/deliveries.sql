fd045ca828ae1e4cb798690e651d1546dfa8fb91
Gonçalo Coito <goncalo.coito@siemens.com>
2026-02-25 11:30:24 +0000

    Lab 11: Fix cancelled status and deprecations

--- models/marts/deliveries.sql
+++ models/marts/deliveries.sql
@@ -30,8 +30,8 @@ final as (
         customer_id,
         count(*) as total_deliveries,
         count(case when delivery_status = 'delivered' then 1 end) as successful_deliveries,
-        count(case when delivery_status = 'failed' then 1 end) as failed_deliveries,
-        count(case when delivery_status not in ('delivered', 'failed') then 1 end) as other_status_deliveries,
+        count(case when delivery_status = 'cancelled' then 1 end) as failed_deliveries,
+        count(case when delivery_status not in ('delivered', 'cancelled') then 1 end) as other_status_deliveries,
         max(delivered_at)::date as last_delivery_date,
         round(successful_deliveries / nullif(total_deliveries, 0), 6) as fulfillment_ratio
     from joined
