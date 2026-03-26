581345b0625881d36cde6f316b3ee02c9bfba311
Matthias Lückl <matthias.lueckl@siemens.com>
2026-03-26 11:00:38 +0000

    Lab 11 completed

---
+++ models/marts/deliveries.sql
@@ -0,0 +1,39 @@
+
+with deliveries as (
+    select
+        *
+    from {{ ref('stg_ecomm__deliveries') }}
+),
+
+orders as (
+    select
+        order_id,
+        customer_id
+    from {{ ref('stg_ecomm__orders') }}
+),
+
+joined as (
+    select
+        orders.customer_id,
+        deliveries.delivery_status,
+        deliveries.delivered_at
+    from deliveries
+    inner join orders using (order_id)
+),
+
+aggregated as (
+    select
+        customer_id,
+        count(*) as total_deliveries,
+        count(case when delivery_status = 'delivered' then 1 end) as successful_deliveries,
+        count(case when delivery_status = 'cancelled' then 1 end) as failed_deliveries,
+        count(case when delivery_status not in ('delivered', 'cancelled') then 1 end) as other_status_deliveries,
+        max(delivered_at)::date as last_delivery_date,
+        coalesce(count(case when delivery_status = 'delivered' then 1 end) / nullif(count(case when delivery_status != 'picked_up' then 1 end), 0),0) as fulfillment_ratio
+    from joined
+    group by 1
+)
+
+select
+    *
+from aggregated
